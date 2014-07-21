8fcf8a8c0a7ddde030b5bdd1aacccd673ee11784
Arroyocode <garroyo@arroyocode.com>
2014-07-20 23:53:40 -0700

    Plunker project file conversion; Initial checkin

---
+++ js/app.js
@@ -0,0 +1,22 @@
+(function() {
+
+    var app = angular.module("githubExplorer", ["ngRoute"]);
+
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when("/main", {
+                templateUrl: "view/main.html",
+                controller: "MainController"
+            })
+            .when("/user/:username", {
+                templateUrl: "view/user.html",
+                controller: "UserController"
+            })
+            .when("/repo/:username/:reponame", {
+                templateUrl: "view/repo.html",
+                controller: "RepoController"
+            })
+            .otherwise({redirectTo: "/main"});
+    });
+
+})();
